1a6ff4e1f4add7a2765f56252e100fff9e3a08df
Ahmet Eren Tomurcuk <54647908+erentomurcuk@users.noreply.github.com>
2024-01-03 12:25:34 +0300

    vid-half

--- cpu6502.h
+++ cpu6502.h
@@ -68,7 +68,7 @@ struct CPU {
         // Set IO vectors
     }
 
-    Byte fetchByte(u32 &cycles, MEM &memory) {
+    Byte fetchByte(u32 &cycles,const MEM &memory) {
         Byte data = memory[PC];
         PC++;
         cycles--;
@@ -81,6 +81,11 @@ struct CPU {
         return data;
     }
 
+    static void writeByte(Byte val, u32 &cycles, Word address, MEM &memory) {
+        memory[address] = val;
+        cycles--;
+    }
+
     static Word readWord(u32 &cycles, Word address, MEM &memory) {
         Byte lowByte = readByte(cycles, address, memory);
         Byte highByte = readByte(cycles, address + 1, memory);
@@ -88,7 +93,7 @@ struct CPU {
         return lowByte | (highByte << 8);
     }
 
-    Word fetchWord(u32 &cycles, MEM &memory) {
+    Word fetchWord(u32 &cycles,const MEM &memory) {
         // 6502 is little endian!
         Word data = memory[PC];
         PC++;
@@ -107,6 +112,8 @@ struct CPU {
     /* OPCODES */
 
     static constexpr Byte
+
+            // LDA
             INST_LDA_IM     = 0xA9,
             INST_LDA_ZP     = 0xA5,
             INST_LDA_ZPX    = 0xB5,
@@ -116,56 +123,78 @@ struct CPU {
             INST_LDA_INDX   = 0xA1,
             INST_LDA_INDY   = 0xB1,
 
+            // LDX
             INST_LDX_IM     = 0xA2,
             INST_LDX_ZP     = 0xA6,
             INST_LDX_ZPY    = 0xB6,
             INST_LDX_ABS    = 0xAE,
             INST_LDX_ABSY   = 0xBE,
 
+            // LDY
             INST_LDY_IM     = 0xA0,
             INST_LDY_ZP     = 0xA4,
             INST_LDY_ZPX    = 0xB4,
             INST_LDY_ABS    = 0xAC,
             INST_LDY_ABSX   = 0xBC,
 
+            // STA
+            INST_STA_ZP     = 0x85,
+            INST_STA_ZPX    = 0x95,
+            INST_STA_ABS    = 0x8D,
+            INST_STA_ABSX   = 0x9D,
+            INST_STA_ABSY   = 0x99,
+            INST_STA_INDX   = 0x81,
+            INST_STA_INDY   = 0x91,
+
+            // STX
+            INST_STX_ZP     = 0x86,
+            INST_STX_ZPX    = 0x96,
+            INST_STX_ABS    = 0x8E,
+
+            // STY
+            INST_STY_ZP     = 0x84,
+            INST_STY_ZPX    = 0x94,
+            INST_STY_ABS    = 0x8C,
+
+            // JSR
             INST_JSR        = 0x20;
 
     // Set correct CPU status after LDA, LDX, LDY
-    void LoadRegisterSetStatus(const Byte reg) {
+    void loadRegisterSetStatus(const Byte reg) {
         Z = (reg == 0);
         N = (reg & 0b10000000) > 0;
     }
 
-    Word addressZeroPage(u32 &cycles, MEM &memory) {
+    Word addressZeroPage(u32 &cycles,const MEM &memory) {
         Byte zeroPageAddress = fetchByte(cycles, memory);
         return zeroPageAddress;
     }
 
-    Word addressZeroPageOffsetX(u32 &cycles, MEM &memory) {
+    Word addressZeroPageOffsetX(u32 &cycles,const MEM &memory) {
         Byte zeroPageAddress = fetchByte(cycles, memory);
         zeroPageAddress += X;
         cycles--;
     }
 
-    Word addressZeroPageOffsetY(u32 &cycles, MEM &memory) {
+    Word addressZeroPageOffsetY(u32 &cycles,const MEM &memory) {
         Byte zeroPageAddress = fetchByte(cycles, memory);
         zeroPageAddress += Y;
         cycles--;
     }
 
-    Word addressAbsolute(u32 &cycles, MEM &memory) {
+    Word addressAbsolute(u32 &cycles,const MEM &memory) {
          Word absoluteAddress = fetchWord(cycles, memory);
          return absoluteAddress;
     }
 
-    Word addressAbsoluteOffsetX(u32 &cycles, MEM &memory) {
+    Word addressAbsoluteOffsetX(u32 &cycles,const MEM &memory) {
         Word absoluteAddress = fetchWord(cycles, memory);
         Word absoluteAddressX = absoluteAddress + X;
         if ((absoluteAddressX - absoluteAddress) >= 0xFF ) cycles--;
         return absoluteAddressX;
     }
 
-    Word addressAbsoluteOffsetY(u32 &cycles, MEM &memory) {
+    Word addressAbsoluteOffsetY(u32 &cycles,const MEM &memory) {
         Word absoluteAddress = fetchWord(cycles, memory);
         Word absoluteAddressY = absoluteAddress + Y;
         if ((absoluteAddressY - absoluteAddress) >= 0xFF ) cycles--;
@@ -173,6 +202,12 @@ struct CPU {
     }
 
     void exec(u32 cycles, MEM &memory) {
+
+        // Load register with the value from a memory address
+        auto loadRegister = [&cycles, &memory, this](Word address, Byte &reg) {
+            reg = readByte(cycles, address, memory);
+            loadRegisterSetStatus(reg);
+        };
 
         while (cycles > 0) {
             Byte inst = fetchByte(cycles, memory);
@@ -184,47 +219,42 @@ struct CPU {
                 case INST_LDA_IM: {
 
                     A = fetchByte(cycles, memory);
-                    LoadRegisterSetStatus(A);
+                    loadRegisterSetStatus(A);
 
                 } break;
 
                 case INST_LDA_ZP: {
 
                     Word address = addressZeroPage(cycles, memory);
-                    A = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(A);
+                    loadRegister(address, A);
 
                 } break;
 
                 case INST_LDA_ZPX: {
 
                     Word address = addressZeroPageOffsetX(cycles, memory);
-                    A = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(A);
+                    loadRegister(address, A);
 
                 } break;
 
                 case INST_LDA_ABS: {
 
                     Word address = addressAbsolute(cycles, memory);
-                    A = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(A);
+                    loadRegister(address, A);
 
                 } break;
 
                 case INST_LDA_ABSX: {
 
                     Word address = addressAbsoluteOffsetX(cycles, memory);
-                    A = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(A);
+                    loadRegister(address, A);
 
                 } break;
 
                 case INST_LDA_ABSY: {
 
                     Word address = addressAbsoluteOffsetY(cycles, memory);
-                    A = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(A);
+                    loadRegister(address, A);
 
                 } break;
 
@@ -234,8 +264,7 @@ struct CPU {
                     ZeroPageAddress += X;
                     cycles--;
                     Word effectiveAddress = readWord(cycles, ZeroPageAddress, memory);
-                    A = readByte(cycles, effectiveAddress, memory);
-                    LoadRegisterSetStatus(A);
+                    loadRegister(effectiveAddress, A);
 
                 } break;
 
@@ -244,9 +273,9 @@ struct CPU {
                     Byte ZeroPageAddress = fetchByte(cycles, memory);
                     Word effectiveAddress = readWord(cycles, ZeroPageAddress, memory);
                     Word effectiveAddressY = effectiveAddress + Y;
-                    A = readByte(cycles, effectiveAddressY, memory);
                     if ((effectiveAddressY - effectiveAddress) >= 0xFF ) cycles--;
-                    LoadRegisterSetStatus(A);
+
+                    loadRegister(effectiveAddressY, A);
 
                 } break;
 
@@ -255,39 +284,35 @@ struct CPU {
                 case INST_LDX_IM: {
 
                     X = fetchByte(cycles, memory);
-                    LoadRegisterSetStatus(X);
+                    loadRegisterSetStatus(X);
 
                 } break;
 
                 case INST_LDX_ZP: {
 
                     Word address = addressZeroPage(cycles, memory);
-                    X = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(X);
+                    loadRegister(address, X);
 
                 } break;
 
                 case INST_LDX_ZPY: {
 
                     Word address = addressZeroPageOffsetY(cycles, memory);
-                    X = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(X);
+                    loadRegister(address, X);
 
                 } break;
 
                 case INST_LDX_ABS: {
 
                     Word address = addressAbsolute(cycles, memory);
-                    X = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(X);
+                    loadRegister(address, X);
 
                 } break;
 
                 case INST_LDX_ABSY: {
 
                     Word address = addressAbsoluteOffsetY(cycles, memory);
-                    X = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(X);
+                    loadRegister(address, X);
 
                 } break;
 
@@ -296,39 +321,129 @@ struct CPU {
                 case INST_LDY_IM: {
 
                     Y = fetchByte(cycles, memory);
-                    LoadRegisterSetStatus(Y);
+                    loadRegisterSetStatus(Y);
 
                 } break;
 
                 case INST_LDY_ZP: {
 
                     Word address = addressZeroPage(cycles, memory);
-                    Y = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(Y);
+                    loadRegister(address, Y);
 
                 } break;
 
                 case INST_LDY_ZPX: {
 
                     Word address = addressZeroPageOffsetX(cycles, memory);
-                    Y = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(Y);
-
+                    loadRegister(address, Y);
                 } break;
 
                 case INST_LDY_ABS: {
 
                     Word address = addressAbsolute(cycles, memory);
-                    Y = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(Y);
+                    loadRegister(address, Y);
 
                 } break;
 
                 case INST_LDY_ABSX: {
 
                     Word address = addressAbsoluteOffsetX(cycles, memory);
-                    Y = readByte(cycles, address, memory);
-                    LoadRegisterSetStatus(Y);
+                    loadRegister(address, Y);
+
+                } break;
+
+                /* STA */
+
+                case INST_STA_ZP: {
+
+                    Word address = addressZeroPage(cycles, memory);
+                    writeByte(A, cycles, address, memory);
+
+                } break;
+
+                case INST_STA_ZPX: {
+
+                    Word address = addressZeroPageOffsetX(cycles, memory);
+                    writeByte(A, cycles, address, memory);
+
+                } break;
+
+                case INST_STA_ABS: {
+
+                    Word address = addressAbsolute(cycles, memory);
+                    writeByte(A, cycles, address, memory);
+
+                } break;
+
+                case INST_STA_ABSX: {
+
+                    Word address = addressAbsoluteOffsetX(cycles, memory);
+                    writeByte(A, cycles, address, memory);
+
+                } break;
+
+                case INST_STA_ABSY: {
+
+                    Word address = addressAbsoluteOffsetY(cycles, memory);
+                    writeByte(A, cycles, address, memory);
+
+                } break;
+
+                case INST_STA_INDX: {
+
+
+
+                } break;
+
+                case INST_STA_INDY: {
+
+
+
+                } break;
+
+                /* STX */
+
+                case INST_STX_ZP: {
+
+                    Word address = addressZeroPage(cycles, memory);
+                    writeByte(X, cycles, address, memory);
+
+                } break;
+
+                case INST_STX_ZPX: {
+
+                    Word address = addressZeroPageOffsetX(cycles, memory);
+                    writeByte(X, cycles, address, memory);
+
+                } break;
+
+                case INST_STX_ABS: {
+
+                    Word address = addressAbsolute(cycles, memory);
+                    writeByte(X, cycles, address, memory);
+
+                } break;
+
+                /* STY */
+
+                case INST_STY_ZP: {
+
+                    Word address = addressZeroPage(cycles, memory);
+                    writeByte(Y, cycles, address, memory);
+
+                } break;
+
+                case INST_STY_ZPX: {
+
+                    Word address = addressZeroPageOffsetY(cycles, memory);
+                    writeByte(Y, cycles, address, memory);
+
+                } break;
+
+                case INST_STY_ABS: {
+
+                    Word address = addressAbsolute(cycles, memory);
+                    writeByte(Y, cycles, address, memory);
 
                 } break;
 
